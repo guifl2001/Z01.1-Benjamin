d0d373469e0645f7cdb8d4e5b9cc6fd4f5850bbf
niveaabreu <niveaadl@al.insper.edu.br>
2021-09-22 10:22:35 -0300

    Sobe Register16

--- Projetos/D-LogicaSequencial/src/Register16.vhd
+++ Projetos/D-LogicaSequencial/src/Register16.vhd
@@ -26,6 +26,22 @@ architecture arch of Register16 is
 	end component;
 
 begin
+	reg0 : Register8
+		port map
+		(
+			clock => clock,
+			input => input(7 downto 0),
+			load => load,
+			output => output (7 downto 0)
+		);
 
+		reg1 : Register8
+		port map
+		(
+			clock => clock,
+			input => input(15 downto 8),
+			load => load,
+			output => output (15 downto 8)
+		);
 
 end architecture;
