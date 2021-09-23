f1b0b97337ecb2eeb26828bbfa33ce3f0f620d9b
Guilherme Fontana <guifl2001@hotmail.com>
2021-09-23 18:50:54 -0300

    FlipFlopJK done and test

--- Projetos/D-LogicaSequencial/src/conceitoA/FlipFlopJK.vhd
+++ Projetos/D-LogicaSequencial/src/conceitoA/FlipFlopJK.vhd
@@ -16,6 +16,26 @@ end entity;
 
 architecture arch of FlipFlopJK is
 
+	signal s: std_logic:= '0';
+	signal input: STD_LOGIC_VECTOR(1 downto 0);
+	
 begin
+
+	input <= J & K;
+	
+	process(clock) begin
+		if(rising_edge(clock)) then
+			if input = "01" then
+				s <= '0';
+			elsif input = "10" then
+				s <= '1';
+			elsif input = "11" then
+				s <= not(s);
+			end if;
+		end if;
+	end process;
+			
+	q <= s;
+	notq <= not s;
 
 end architecture;
