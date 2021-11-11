bc581116b82b437095d121faa4fcc826522b1284
niveaabreu <niveaadl@al.insper.edu.br>
2021-11-11 19:04:49 -0300

    funcionado e testado

--- Projetos/G-Assembler/Assembler/src/main/java/assembler/Code.java
+++ Projetos/G-Assembler/Assembler/src/main/java/assembler/Code.java
@@ -2,14 +2,11 @@
  * Curso: Elementos de Sistemas
  * Arquivo: Code.java
  */
-
 package assembler;
-
 /**
  * Traduz mnemônicos da linguagem assembly para códigos binários da arquitetura Z0.
  */
 public class Code {
-
     /**
      * Retorna o código binário do(s) registrador(es) que vão receber o valor da instrução.
      * @param  mnemnonic vetor de mnemônicos "instrução" a ser analisada.
@@ -17,9 +14,68 @@ public class Code {
      */
     public static String dest(String[] mnemnonic) {
         /* TODO: implementar */
-    	return "";
+        if (!mnemnonic[0].equals("movw")) {
+            switch (mnemnonic[mnemnonic.length -1]) {
+                case "%A":
+                    return "0001";
+                case "%D":
+                    return "0010";
+                case "(%A)":
+                    return "0100";
+                default:
+                    return "0000";
+            }
+        }
+        else {
+            if (mnemnonic.length > 3){
+                switch (mnemnonic[mnemnonic.length -1]){
+                    case "%A":
+                        if (mnemnonic[mnemnonic.length -2].equals("(%A)")){
+                            return "0101";
+                        }
+                        else if (mnemnonic[mnemnonic.length - 2].equals("%D")){
+                            return  "0011";
+                        }
+                        else {
+                            return "0000";
+                        }
+                    case "%D":
+                        if (mnemnonic[mnemnonic.length -2].equals("(%A)")){
+                            return "0110";
+                        }
+                        else if (mnemnonic[mnemnonic.length - 2].equals("%A")){
+                            return  "0011";
+                        }
+                        else {
+                            return "0000";
+                        }
+                    case "(%A)":
+                        if (mnemnonic[mnemnonic.length -2].equals("%A")){
+                            return "0101";
+                        }
+                        else if (mnemnonic[mnemnonic.length - 2].equals("%D")){
+                            return  "0110";
+                        }
+                        else {
+                            return "0000";
+                        }
+                }
+            }
+            else {
+                switch (mnemnonic[mnemnonic.length -1]) {
+                    case "%A":
+                        return "0001";
+                    case "%D":
+                        return "0010";
+                    case "(%A)":
+                        return "0100";
+                    default:
+                        return "0000";
+                }
+            }
+        }
+        return "0000";
     }
-
     /**
      * Retorna o código binário do mnemônico para realizar uma operação de cálculo.
      * @param  mnemnonic vetor de mnemônicos "instrução" a ser analisada.
@@ -27,9 +83,97 @@ public class Code {
      */
     public static String comp(String[] mnemnonic) {
         /* TODO: implementar */
-    	return "";
+        switch (mnemnonic[0]) {
+            case "movw":
+                switch (mnemnonic[1]) {
+                    case "%A":
+                        return "000110000";
+                    case "%D":
+                        return "000001100";
+                    case "(%A)":
+                        return "001110000";
+                    case "$1":
+                        return "000111111";
+                    case "$0":
+                        return "000101010";
+                }
+            case "notw":
+                switch (mnemnonic[1]) {
+                    case "%D":
+                        return "000001101";
+                    case "%A":
+                        return "000110001";
+                    case "(%A)":
+                        return "001110001";
+                }
+            case "negw":
+                switch (mnemnonic[1]) {
+                    case "%D":
+                        return "000001111";
+                    case "%A":
+                        return "000110011";
+                    case "(%A)":
+                        return "001110011";
+                }
+            case "incw":
+                switch (mnemnonic[1]) {
+                    case "%A":
+                        return "000110111";
+                    case "%D":
+                        return "000011111";
+                    case "(%A)":
+                        return "001110111";
+                }
+            case "andw":
+                switch (mnemnonic[1]) {
+                    case "(%A)":
+                        return "001000000";
+                    case "%D":
+                    case "%A":
+                        return "000000000";
+                }
+            case "addw":
+                switch (mnemnonic[1]) {
+                    case "%A":
+                    case "%D":
+                        return "000000010";
+                    case "(%A)":
+                        return "001000010";
+                    case "$1":
+                        return "001110111";
+                }
+            case "decw":
+                switch (mnemnonic[1]) {
+                    case "%D":
+                        return "000001110";
+                    case "%A":
+                        return "000110010";
+                    case "(%A)":
+                        return "001110010";
+                }
+            case "orw":
+                switch (mnemnonic[1]) {
+                    case "%D":
+                    case "%A":
+                        return "000010101";
+                    case "(%A)":
+                        return "001010101";
+                }
+            case "subw":
+                switch (mnemnonic[1]) {
+                    case "%D":
+                        return "001010011";
+                    case "(%A)":
+                        return "001110010";
+                }
+            case "rsubw":
+                if ("%D".equals(mnemnonic[1])) {
+                    return "001000111";
+                }
+            default:
+                return "000001100";
+        }
     }
-
     /**
      * Retorna o código binário do mnemônico para realizar uma operação de jump (salto).
      * @param  mnemnonic vetor de mnemônicos "instrução" a ser analisada.
@@ -37,9 +181,25 @@ public class Code {
      */
     public static String jump(String[] mnemnonic) {
         /* TODO: implementar */
-    	return "";
+        switch (mnemnonic[0]) {
+            case "jg":
+                return "001";
+            case "je":
+                return "010";
+            case "jge":
+                return "011";
+            case "jl":
+                return "100";
+            case "jne":
+                return "101";
+            case "jle":
+                return "110";
+            case "jmp":
+                return "111";
+            default:
+                return "000";
+        }
     }
-
     /**
      * Retorna o código binário de um valor decimal armazenado numa String.
      * @param  symbol valor numérico decimal armazenado em uma String.
@@ -47,7 +207,8 @@ public class Code {
      */
     public static String toBinary(String symbol) {
         /* TODO: implementar */
-    	return "";
+        int symbol_value = Integer.parseInt(symbol);
+        String binary = Integer.toBinaryString(symbol_value);
+        return String.format("%1$16s", binary).replace(" ", "0");
     }
-
 }
